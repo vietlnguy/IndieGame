2ff751c6f971973e3d3d7c1dca91c8d381c4ba8f
Viet <viet.nguy.95@gmail.com>
2026-03-19 15:18:34 -0400

    update

---
+++ Assets/Scripts/Chapter2/Chapter2.cs
@@ -0,0 +1,201 @@
+using UnityEngine;
+
+public class Chapter2 : MonoBehaviour
+{
+    public GameObject basicEnemyPrefab;
+    private GameObject enemies;
+    public BattleController battleController;
+    private bool enemiesSpawned = false;
+    private bool shouldLose = false;
+    private bool gameOver = false;
+    private bool victorySequenceStarted = false;
+    private SaveManager saveManager;
+    public GameOver gameOverScript;
+    public AttackPreview attackPreviewScript;
+    public SubquestsBox subquestsBoxScript;
+    public GameObject victorySequence;
+    public GameObject mainCharacterPrefab;
+    public GameObject astridPrefab;
+    private GameObject characters;
+
+    public void Awake()
+    {    
+    
+        saveManager = FindFirstObjectByType<SaveManager>();
+        characters = GameObject.Find("Characters");
+        enemies = GameObject.Find("Enemies");
+
+        foreach (Character character in saveManager.loadedData.characters)
+        {
+            if (character.characterName == saveManager.loadedData.mainCharacterName)
+            {
+                Instantiate(mainCharacterPrefab, new Vector3(-20f, -9.35f), Quaternion.identity, characters.transform);
+            }
+            else if (character.characterName == "Astrid")
+            {
+                Instantiate(astridPrefab, new Vector3(-4.45f, -11.65f), Quaternion.identity, characters.transform);
+            }
+        }
+
+    }
+    public void Update()
+    {
+        //Can script reinforcements, mid combat dialogues, etc.
+        //Battle controller should be abstract enough to apply to all chapters
+        //Chapter specific script events happen here, and win/lose conditions
+        
+        //Win condition
+        if (battleController.enemies.transform.childCount == 0 && enemiesSpawned && !victorySequenceStarted && !attackPreviewScript.coroutineRunning)
+        {
+            //Start outro scene
+            battleController.CancelEveryting();
+            victorySequence.SetActive(true);
+            StartCoroutine(victorySequence.GetComponent<VictorySequence>().Victory());
+            enemiesSpawned = false; //remove later
+            victorySequenceStarted = true;
+        }
+        
+        //Lose condition
+        if (shouldLose && !gameOver && !attackPreviewScript.coroutineRunning)
+        {
+            battleController.CancelEveryting();
+            gameOver = true;
+            StartCoroutine(gameOverScript.GameOverSequence());
+
+        }
+
+    }
+    public void CreateEnemies()
+    {
+        BasicRangedEnemy(-21f, -11.25f, 0f);
+        BasicEnemy(-9f, -14.24f, 0f);
+        BasicEnemy(2.57f, -10.15f, 0f);
+        BossEnemy(10.5f, -6.36f, 0f);
+        enemiesSpawned = true;
+        CharacterDeathSubscribe(); 
+        EnemyDeathSubscribe();
+
+    }
+    public void BasicEnemy(float x, float y, float z)
+    {
+        GameObject temp = Instantiate(basicEnemyPrefab, new Vector3(x, y, z), Quaternion.identity, enemies.transform);
+        EnemyController enemy = temp.GetComponent<EnemyController>();
+        enemy.title = "Soldier";
+
+        enemy.maxHp = 11;
+        enemy.currentHp = enemy.maxHp;
+        enemy.maxMana = 8;
+        enemy.currentMana = enemy.maxMana;
+        enemy.attack = 7;
+        enemy.defense = 4;
+        enemy.resistance = 3;
+        enemy.intelligence = 4;
+        enemy.skill = 5;
+        enemy.speed = 4;
+        enemy.attackRange = 1;
+        enemy.moveRange = 4;
+
+        enemy.roams = true;
+        enemy.ranged = false;
+        enemy.support = false;
+        enemy.hybrid = false;
+
+        enemy.knownAttacks.Add(new Attack("Bash", "physical", 1.0f, 1.0f, 90, 0, 0, "Bash the enemy with your weapon."));
+    }
+    public void BasicRangedEnemy(float x, float y, float z)
+    {
+        GameObject temp = Instantiate(basicEnemyPrefab, new Vector3(x, y, z), Quaternion.identity, enemies.transform);
+        EnemyController enemy = temp.GetComponent<EnemyController>();
+        enemy.title = "Soldier";
+
+        enemy.maxHp = 11;
+        enemy.currentHp = enemy.maxHp;
+        enemy.maxMana = 8;
+        enemy.currentMana = enemy.maxMana;
+        enemy.attack = 7;
+        enemy.defense = 4;
+        enemy.resistance = 3;
+        enemy.intelligence = 4;
+        enemy.skill = 5;
+        enemy.speed = 4;
+        enemy.attackRange = 3;
+        enemy.moveRange = 4;
+
+        enemy.roams = true;
+        enemy.ranged = true;
+        enemy.support = false;
+        enemy.hybrid = false;
+
+        enemy.knownAttacks.Add(new Attack("Bow Shot", "physical", 1.0f, 1.0f, 90, 0, 0, "Shoot at arrow at the enemy."));
+    }
+    public void BossEnemy(float x, float y, float z)
+    {
+        GameObject temp = Instantiate(basicEnemyPrefab, new Vector3(x, y, z), Quaternion.identity, enemies.transform);
+        EnemyController enemy = temp.GetComponent<EnemyController>();
+        enemy.title = "Hegseth";
+
+        enemy.maxHp = 14;
+        enemy.currentHp = enemy.maxHp;
+        enemy.maxMana = 9;
+        enemy.currentMana = enemy.maxMana;
+        enemy.attack = 8;
+        enemy.defense = 5;
+        enemy.resistance = 3;
+        enemy.intelligence = 4;
+        enemy.skill = 6;
+        enemy.speed = 4;
+        enemy.attackRange = 1;
+        enemy.moveRange = 4;
+
+        enemy.roams = false;
+        enemy.ranged = false;
+        enemy.boss = true;
+        enemy.deathDialogue = "Gah-- I must fall back. You will regret this. King Reiss WILL have your relic...";
+        enemy.knownAttacks.Add(new Attack("Bash", "physical", 1.1f, 1.0f, 90, 0, 0, "Bash the enemy with your weapon."));
+
+    }
+    private void CharacterDeathSubscribe()
+    {
+        PlayerController.OnCharacterDied += HandleDeath;
+    }
+    private void CharacterDeathUnsubscribe()
+    {
+        PlayerController.OnCharacterDied -= HandleDeath;
+    }
+    private void HandleDeath(string name)
+    {
+        Debug.Log("Heard that " + name + " died!");
+        if (name == "Astrid" || name == saveManager.loadedData.mainCharacterName)
+        {
+            shouldLose = true;
+        }
+
+    }
+    private void EnemyDeathSubscribe()
+    {
+        EnemyController.OnEnemyDied += HandleEnemyDeath;
+    }
+    private void EnemyDeathUnsubscribe()
+    {
+        EnemyController.OnEnemyDied -= HandleEnemyDeath;
+    }
+    private void HandleEnemyDeath(GameObject[] list)
+    {
+        Debug.Log("Heard that " + list[0].GetComponent<EnemyController>().title + " was killed by " + list[1].GetComponent<PlayerController>().title);
+
+        //Subquest 1: Astrid lands killing blow on boss
+        if (list[0].GetComponent<EnemyController>().boss && list[1].GetComponent<PlayerController>().title != "Astrid")
+        {
+            //Quest failure
+            subquestsBoxScript.updateQuest("Astrid1", false, list[1]);
+        }
+        else if (list[0].GetComponent<EnemyController>().boss && list[1].GetComponent<PlayerController>().title == "Astrid")
+        {
+            //Quest success
+            subquestsBoxScript.updateQuest("Astrid1", true, list[1]);
+
+        }
+
+    }
+
+}
