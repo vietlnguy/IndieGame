1fb13f24f05df8586a6cc95aeab0f123c0b4360b
Viet <viet.nguy.95@gmail.com>
2026-05-24 10:25:04 -0400

    volcano, mountain, highlands, desert, ruins

---
+++ Assets/Environments/EpicRPGWorlds/Highlands/Epic RPG World - Highlands V1.4.1/TiledMap Editor/Tilesets/Highlands-Tileset - platformV2-3tiles tall.tsx
@@ -0,0 +1,340 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="platformV2-3t" tilewidth="32" tileheight="32" tilecount="259" columns="16">
+ <image source="../../tilesets and props/platformV2-3 tiles tall.png" width="512" height="512"/>
+ <tile id="0"/>
+ <tile id="1" probability="0"/>
+ <tile id="2"/>
+ <tile id="3" probability="0"/>
+ <tile id="4"/>
+ <tile id="5"/>
+ <tile id="6"/>
+ <tile id="7"/>
+ <tile id="8"/>
+ <tile id="9" probability="0"/>
+ <tile id="10" probability="0"/>
+ <tile id="11"/>
+ <tile id="12"/>
+ <tile id="13"/>
+ <tile id="14"/>
+ <tile id="15"/>
+ <tile id="16"/>
+ <tile id="17"/>
+ <tile id="18" probability="0.5"/>
+ <tile id="19"/>
+ <tile id="20"/>
+ <tile id="21"/>
+ <tile id="22"/>
+ <tile id="23"/>
+ <tile id="24"/>
+ <tile id="25"/>
+ <tile id="26"/>
+ <tile id="27"/>
+ <tile id="28"/>
+ <tile id="29"/>
+ <tile id="30"/>
+ <tile id="31"/>
+ <tile id="32">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="0.076555" width="16" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="33"/>
+ <tile id="34"/>
+ <tile id="35"/>
+ <tile id="36"/>
+ <tile id="37"/>
+ <tile id="38"/>
+ <tile id="39"/>
+ <tile id="40"/>
+ <tile id="41"/>
+ <tile id="42"/>
+ <tile id="43"/>
+ <tile id="44"/>
+ <tile id="45"/>
+ <tile id="46"/>
+ <tile id="47"/>
+ <tile id="48">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="0" width="16" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="49"/>
+ <tile id="50"/>
+ <tile id="51"/>
+ <tile id="52"/>
+ <tile id="53"/>
+ <tile id="54"/>
+ <tile id="55"/>
+ <tile id="56"/>
+ <tile id="57"/>
+ <tile id="58"/>
+ <tile id="59"/>
+ <tile id="60"/>
+ <tile id="61"/>
+ <tile id="62"/>
+ <tile id="63"/>
+ <tile id="64">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="0" width="16" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="65" probability="0"/>
+ <tile id="66"/>
+ <tile id="67" probability="0"/>
+ <tile id="68"/>
+ <tile id="69"/>
+ <tile id="70"/>
+ <tile id="71"/>
+ <tile id="72"/>
+ <tile id="73"/>
+ <tile id="74"/>
+ <tile id="75"/>
+ <tile id="76"/>
+ <tile id="77"/>
+ <tile id="78"/>
+ <tile id="79"/>
+ <tile id="80">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="0" width="16" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="81"/>
+ <tile id="82"/>
+ <tile id="83"/>
+ <tile id="84"/>
+ <tile id="85"/>
+ <tile id="86"/>
+ <tile id="87"/>
+ <tile id="88"/>
+ <tile id="89"/>
+ <tile id="90"/>
+ <tile id="91"/>
+ <tile id="92"/>
+ <tile id="93"/>
+ <tile id="94"/>
+ <tile id="95"/>
+ <tile id="96"/>
+ <tile id="97"/>
+ <tile id="98"/>
+ <tile id="99"/>
+ <tile id="100"/>
+ <tile id="101"/>
+ <tile id="102"/>
+ <tile id="103"/>
+ <tile id="104"/>
+ <tile id="105"/>
+ <tile id="106"/>
+ <tile id="107"/>
+ <tile id="108"/>
+ <tile id="109"/>
+ <tile id="110"/>
+ <tile id="111"/>
+ <tile id="112"/>
+ <tile id="113"/>
+ <tile id="114"/>
+ <tile id="115"/>
+ <tile id="116"/>
+ <tile id="117"/>
+ <tile id="118"/>
+ <tile id="119"/>
+ <tile id="120"/>
+ <tile id="121"/>
+ <tile id="122"/>
+ <tile id="123"/>
+ <tile id="124"/>
+ <tile id="125"/>
+ <tile id="126"/>
+ <tile id="127"/>
+ <tile id="128"/>
+ <tile id="129"/>
+ <tile id="130"/>
+ <tile id="131"/>
+ <tile id="132"/>
+ <tile id="133"/>
+ <tile id="134"/>
+ <tile id="135"/>
+ <tile id="136"/>
+ <tile id="137"/>
+ <tile id="138"/>
+ <tile id="139"/>
+ <tile id="140"/>
+ <tile id="141"/>
+ <tile id="142"/>
+ <tile id="143"/>
+ <tile id="144" probability="0"/>
+ <tile id="145" probability="0"/>
+ <tile id="146" probability="0"/>
+ <tile id="147" probability="0"/>
+ <tile id="148" probability="0"/>
+ <tile id="149" probability="0"/>
+ <tile id="150"/>
+ <tile id="151" probability="0"/>
+ <tile id="152" probability="0"/>
+ <tile id="153" probability="0"/>
+ <tile id="154" probability="0"/>
+ <tile id="155" probability="0"/>
+ <tile id="156" probability="0"/>
+ <tile id="157"/>
+ <tile id="158"/>
+ <tile id="159"/>
+ <tile id="160"/>
+ <tile id="161"/>
+ <tile id="162"/>
+ <tile id="163"/>
+ <tile id="164"/>
+ <tile id="165"/>
+ <tile id="166"/>
+ <tile id="167"/>
+ <tile id="168"/>
+ <tile id="169"/>
+ <tile id="170"/>
+ <tile id="171"/>
+ <tile id="172"/>
+ <tile id="173"/>
+ <tile id="174"/>
+ <tile id="175"/>
+ <tile id="176"/>
+ <tile id="177"/>
+ <tile id="178"/>
+ <tile id="179"/>
+ <tile id="180"/>
+ <tile id="181"/>
+ <tile id="182"/>
+ <tile id="183"/>
+ <tile id="184"/>
+ <tile id="185"/>
+ <tile id="186"/>
+ <tile id="187"/>
+ <tile id="188"/>
+ <tile id="189"/>
+ <tile id="190"/>
+ <tile id="191"/>
+ <tile id="192" probability="0"/>
+ <tile id="193" probability="0"/>
+ <tile id="194" probability="0"/>
+ <tile id="195" probability="0"/>
+ <tile id="196" probability="0"/>
+ <tile id="197" probability="0"/>
+ <tile id="198" probability="0"/>
+ <tile id="199" probability="0"/>
+ <tile id="200" probability="0"/>
+ <tile id="201" probability="0"/>
+ <tile id="202" probability="0"/>
+ <tile id="203" probability="0"/>
+ <tile id="204" probability="0"/>
+ <tile id="205"/>
+ <tile id="206"/>
+ <tile id="207"/>
+ <tile id="208" probability="0"/>
+ <tile id="209" probability="0"/>
+ <tile id="210" probability="0"/>
+ <tile id="211" probability="0"/>
+ <tile id="212" probability="0"/>
+ <tile id="213"/>
+ <tile id="214"/>
+ <tile id="215"/>
+ <tile id="216"/>
+ <tile id="217"/>
+ <tile id="218"/>
+ <tile id="219"/>
+ <tile id="220"/>
+ <tile id="221"/>
+ <tile id="222"/>
+ <tile id="223"/>
+ <tile id="224" probability="0"/>
+ <tile id="225" probability="0"/>
+ <tile id="226" probability="0"/>
+ <tile id="227" probability="0"/>
+ <tile id="228" probability="0"/>
+ <tile id="229"/>
+ <tile id="230"/>
+ <tile id="231"/>
+ <tile id="232"/>
+ <tile id="233"/>
+ <tile id="234"/>
+ <tile id="235"/>
+ <tile id="236"/>
+ <tile id="237"/>
+ <tile id="238"/>
+ <tile id="239"/>
+ <tile id="240"/>
+ <tile id="241"/>
+ <tile id="242"/>
+ <tile id="243"/>
+ <tile id="244"/>
+ <tile id="245"/>
+ <tile id="246"/>
+ <tile id="247"/>
+ <tile id="248"/>
+ <tile id="249"/>
+ <tile id="250"/>
+ <tile id="251"/>
+ <tile id="252"/>
+ <tile id="253"/>
+ <tile id="254"/>
+ <tile id="255"/>
+ <tile id="265"/>
+ <tile id="276"/>
+ <tile id="281"/>
+ <wangsets>
+  <wangset name="platformV2-3tiles tall" type="corner" tile="66">
+   <wangcolor name="" color="#ff0000" tile="66" probability="1"/>
+   <wangtile tileid="1" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="3" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="9" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="10" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="16" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="17" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="18" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="19" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="20" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="32" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="33" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="34" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="35" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="36" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="48" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="49" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="50" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="51" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="52" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="65" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="66" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="67" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="144" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="145" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="146" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="147" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="148" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="149" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="151" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="152" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="153" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="154" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="155" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="156" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="192" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="193" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="194" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="195" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="196" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="197" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="198" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="199" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="200" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="201" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="202" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="203" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="204" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="208" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="209" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="210" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="211" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="212" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="226" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="227" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="228" wangid="0,0,0,1,0,1,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
