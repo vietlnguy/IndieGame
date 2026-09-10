0bde40248ce9f74aad48b27d2f8330b77a051f77
vietnguy <viet.nguy.95@gmail.com>
2026-09-10 11:53:09 -0400

    prologue localization

--- Assets/Scripts/Prologue/PrologueController.cs
+++ Assets/Scripts/Prologue/PrologueController.cs
@@ -26,6 +26,7 @@ public class PrologueController : MonoBehaviour
     public GameObject skipTextBox;
     private Coroutine coroutineRunning;
     public GameObject environmentImage;
+    private DialogueWrapper allDialogues;
 
     void Awake(){
         AudioListener.volume = PlayerPrefs.GetFloat("volume", 0.5f);
@@ -41,21 +42,12 @@ public class PrologueController : MonoBehaviour
     }
     void Start()
     {
-        dialogues.Add("Before the age of man...");
-        dialogues.Add("... there were only beings of nature.");
-        dialogues.Add("Spirits, nymphs, and early mankind living in perfect harmony.");
-        dialogues.Add("For a thousand years they grew, sharing knowledge and wisdom.");
-        dialogues.Add("... and pleasure.");
-        dialogues.Add("Eventually, these beings became indistinguishable from one another, and thus adopted a new name.. ");
-        dialogues.Add("The Tah'Lo.");
-        dialogues.Add("An extension of nature and technology, the Tah'Lo were masters of the weave.");
-        dialogues.Add("Bestowing magic into artifacts and each other alike, the Tah'Lo enjoyed an age of prosperity.");
-        dialogues.Add("But dark times loomed ahead...");
-        dialogues.Add("For some, it was not enough.");
-        dialogues.Add("The lure of power was all it took to topple the mighty civilization.");
-        dialogues.Add("Millenia have since passed, and tales of the Tah'Lo have turned into legend.");
-        dialogues.Add("But once again the power of the Tah'Lo threaten the stablity of the realm.");
-        dialogues.Add("On a small farm in the countryside, an unlikely adventure begins.");
+
+        foreach (DialogueEntry entry in allDialogues.dialogueEntries) {
+            foreach (Line line in entry.lines) {
+                dialogues.Add(line.line);
+            }
+        }
 
         StartCoroutine(Intro());
     }
